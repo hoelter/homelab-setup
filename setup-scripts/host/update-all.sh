548d2cacbd76b184dc2ac82422036f1c06075e8f
Christopher Hoelter <git@christopherhoelter.com>
2025-11-29 18:29:18 -0600

    Quick commit

--- setup-scripts/host/update-all.sh
+++ setup-scripts/host/update-all.sh
@@ -2,12 +2,25 @@
 
 set -e
 
-echo "Starting host udpates..."
-sudo apt update && sudo apt full-upgrade -y
+TIMESTAMP=$(date +"%Y-%m-%d-%H%M")
 
 echo "Starting container udpates..."
+
+incus snapshot create git "updateall-$TIMESTAMP"
 incus exec git -- sh -c "apt update && apt full-upgrade -y"
+
+incus snapshot create nas "updateall-$TIMESTAMP"
 incus exec nas -- sh -c "apt update && apt full-upgrade -y"
+
+incus snapshot create paperless "updateall-$TIMESTAMP"
 incus exec paperless -- sh -c "apt update && apt full-upgrade -y"
+
+incus snapshot create jellyfin "updateall-$TIMESTAMP"
 incus exec jellyfin -- sh -c "apt update && apt full-upgrade -y"
+
+incus snapshot create torrent "updateall-$TIMESTAMP"
 incus exec torrent -- sh -c "apt update && apt full-upgrade -y"
+
+echo "Starting host updates..."
+sudo apt update && sudo apt full-upgrade -y
+
